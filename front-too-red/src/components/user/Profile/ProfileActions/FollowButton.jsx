010eec7418d4e94f828d53ec7c5ae155ffd18861
Antonio Gómez <antgomdgz@gmail.com>
2024-12-26 12:54:54 +0100

    Botón follow en Profile

--- front-too-red/src/components/user/Profile/ProfileActions/FollowButton.jsx
+++ front-too-red/src/components/user/Profile/ProfileActions/FollowButton.jsx
@@ -49,13 +49,14 @@ const FollowButton = ({ userId, iFollow, setIFollow, token, showToastMessage }) 
   return (
     <button
       onClick={iFollow ? unFollow : follow}
-      className={`flex items-center bg-white border-2 ${
-        iFollow ? "text-red-600 border-gray-900" : "text-gray-900 border-red-600"
-      } font-semibold px-2 py-2 rounded-lg shadow-md hover:scale-110 duration-300 transition-all w-auto h-10`}
+      className={`flex items-center border-2 ${
+        iFollow ? "bg-red-600 text-white hover:bg-red-700"
+                        : "bg-gray-300 text-gray-900 hover:bg-gray-400"
+      } font-semibold px-2 py-2 rounded-full shadow-md hover:scale-110 duration-300 transition-all w-auto h-10`}
     >
       {iFollow ? (
         <>
-          <UserMinusIcon className="w-5 h-5 md:mr-2 text-red-600" />
+          <UserMinusIcon className="w-5 h-5 md:mr-2 text-white" />
           <span className="hidden md:inline">Dejar de seguir</span>
         </>
       ) : (
